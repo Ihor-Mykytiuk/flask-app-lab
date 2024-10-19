b712419b60c1bfdab07f8e2fc3c598e19ba28483
Ihor Mykytiuk <igor14vn@gmail.com>
2024-10-19 14:36:50 +0300

    implemented url_for usage and examples of dynamic pages

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template, abort
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 @app.route('/')
 def main():
-    return 'Hello, World!'
+    return render_template('hello.html')
 
 @app.route('/homepage')
 def home():
     """View for the Home page of your website."""
     agent = request.user_agent
-    return f"<h1>This is your homepage :) - {agent}</h1>"
+    return render_template('home.html', agent=agent)
 
 @app.route('/hi/<string:name>/')
 def greetings(name):
     name = name.upper()
     age = request.args.get('age', 0, type=int)
-    return f"Welcome {name=} {age=}", 200
+    return render_template('hi.html', name=name, age=age)
 
 @app.route('/admin')
 def admin():
     to_url = url_for("greetings", name="administrator", _external=True)
     return redirect(to_url)
+
+posts = [
+    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+]
+
+@app.route('/posts')
+def get_posts():
+    return render_template('posts.html', posts=posts)
+
+@app.route('/post/<int:id>')
+def get_post(id):
+    if id < 1 or id > len(posts):
+        abort(404)
+    post = posts[id-1]
+    return render_template('detail_post.html', post=post)
 
 if __name__ == '__main__':
     app.run(debug=True)
