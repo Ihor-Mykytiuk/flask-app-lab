a9584e31775d9e15ce69a762af45b1c097bbeff5
Ihor Mykytiuk <igor14vn@gmail.com>
2024-11-09 19:16:25 +0200

    404 error handler + stylized page

--- app/views.py
+++ app/views.py
@@ -15,3 +15,7 @@ def home():
 @app.route('/resume')
 def resume():
     return render_template('resume.html', title='Резюме')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
