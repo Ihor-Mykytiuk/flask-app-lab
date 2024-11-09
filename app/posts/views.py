0ca2386e000033097968fc287eb5a2baecc9e9f2
Ihor Mykytiuk <igor14vn@gmail.com>
2024-11-09 19:05:55 +0200

    Processing adding post + displaying posts as cards

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,12 +1,23 @@
+import os, json
 from . import posts_bp
-from flask import render_template, abort, flash, redirect, url_for
+from flask import render_template, abort, flash, redirect, url_for, session
 from .forms import PostForm
 
-posts = [
-    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
-    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
-    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-]
+POST_FILE = 'app/posts/posts.json'
+
+
+def load_json_data(file):
+    if not os.path.exists(file):
+        return []
+    with open(file, 'r') as f:
+        data = json.load(f)
+        return data
+
+def save_json_data(file, data):
+    with open(file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+posts = load_json_data(POST_FILE)
 
 @posts_bp.route('/')
 def get_posts():
@@ -16,14 +27,24 @@ def get_posts():
 def add_post():
     form = PostForm()
     if form.validate_on_submit():
-        title = form.title.data
-        flash(f"Post '{title}' has been added.", 'success')
+        new_post = {
+            "id": len(posts) + 1,
+            "title": form.title.data,
+            "content": form.content.data,
+            "is_active": form.is_active.data,
+            "published_date": form.publish_date.data.strftime('%Y-%m-%d'),
+            "category": form.category.data,
+            "author": session.get('username', 'Anonymous')
+        }
+        posts.append(new_post)
+        save_json_data(POST_FILE, posts)
+        flash(f"Post '{new_post['title']}' has been added.", 'success')
         return redirect(url_for('.get_posts'))
     return render_template('add_post.html', form=form)
 
-@posts_bp.route('/<int:id>')
-def get_post(id):
-    if id < 1 or id > len(posts):
+@posts_bp.route('/<int:post_id>')
+def get_post(post_id):
+    if post_id < 1 or post_id > len(posts):
         abort(404)
-    post = posts[id-1]
+    post = posts[post_id - 1]
     return render_template('detail_post.html', post=post)
